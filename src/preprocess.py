cd5d121fe9ae7e7c658bd587ac77483ba64a7c33
ThankGod Emmanuel <thankgdofem219@gmail.com>
2026-05-15 18:13:45 +0100

    run dataset for inspection

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,8 +6,10 @@ df = pd.read_csv("data/student_performance.csv")
 # Show first rows
 print(df.head())
 
-# Shows dataset info. This enables us inspect columns, identify target variable, clean the dataset properly before training
+# Shows dataset info.
 print(df.info())
 
 # Check missing values
 print(df.isnull().sum())
+
+# This enables us inspect columns, identify target variable, clean the dataset properly before training
